2e2dbfecb8565d30d032fe068ac49b0a7969c41e
alexchoicy <alex123321choicy@gmail.com>
2024-04-06 23:08:19 +0800

    update User structure

--- src/main/java/com/cems/database/UserManager.java
+++ src/main/java/com/cems/database/UserManager.java
@@ -3,16 +3,13 @@ package com.cems.database;
 import com.cems.Enums.UserRoles;
 import com.cems.Model.Users;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class UserManager extends DatabaseManager {
     public ArrayList<Users> getUsers() {
         try (Connection connection = getConnection()) {
-            String sql = "SELECT * FROM users";
+            String sql = "SELECT * FROM user";
             System.out.println("sql: " + sql);
             Statement statement = connection.createStatement();
 
@@ -24,13 +21,7 @@ public class UserManager extends DatabaseManager {
             ResultSet resultSet = statement.getResultSet();
             ArrayList<Users> users = new ArrayList<>();
             while (resultSet.next()) {
-                Users user = new Users();
-                user.setUserId(resultSet.getInt("userId"));
-                user.setUsername(resultSet.getString("username"));
-                user.setPhoneNumber(resultSet.getString("phoneNumber"));
-                user.setFirstName(resultSet.getString("firstName"));
-                user.setLastName(resultSet.getString("lastName"));
-                user.setRole(UserRoles.getRoles(resultSet.getInt("role")));
+                Users user = GetUserDataToBean(resultSet);
                 users.add(user);
             }
             resultSet.close();
@@ -46,29 +37,33 @@ public class UserManager extends DatabaseManager {
 
     public Users Login(String username, String password) {
         try (Connection connection = getConnection()) {
-            String sql = "SELECT * FROM users WHERE username = ? AND password = ?;";
+            String sql = "SELECT * FROM user WHERE username = ? AND password = ?;";
             PreparedStatement statement = connection.prepareStatement(sql);
 
             statement.setString(1, username);
             statement.setString(2, password);
 
             ResultSet resultSet = statement.executeQuery();
-            Users user = new Users();
+            Users user;
             if (resultSet.next()) {
-                user.setUserId(resultSet.getInt("userId"));
-                user.setUsername(resultSet.getString("username"));
-                user.setPhoneNumber(resultSet.getString("phoneNumber"));
-                user.setFirstName(resultSet.getString("firstName"));
-                user.setLastName(resultSet.getString("lastName"));
-                user.setRole(UserRoles.getRoles(resultSet.getInt("role")));
-            } else {
-                return null;
+                user = GetUserDataToBean(resultSet);
+                return user;
             }
-            return user;
         } catch (Exception e) {
 //            e.printStackTrace();
             System.out.println("Error: " + e.getMessage());
         }
         return null;
+    }
+
+    private Users GetUserDataToBean(ResultSet resultSet) throws SQLException {
+        Users user = new Users();
+        user.setUserId(resultSet.getInt("user_id"));
+        user.setUsername(resultSet.getString("username"));
+        user.setPhoneNumber(resultSet.getString("phone_number"));
+        user.setFirstName(resultSet.getString("first_name"));
+        user.setLastName(resultSet.getString("last_name"));
+        user.setRole(UserRoles.getRoles(resultSet.getInt("role")));
+        return user;
     }
 }
