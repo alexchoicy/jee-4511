831fb289fd85d1bcabcaf5a8309e843f54f23593
alexchoicy <alex123321choicy@gmail.com>
2024-04-20 02:02:57 +0800

    parse date time function

--- src/main/java/com/cems/Utils/ParseUtil.java
+++ src/main/java/com/cems/Utils/ParseUtil.java
@@ -1,5 +1,9 @@
 package com.cems.Utils;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ParseUtil {
     public static int tryParseInt(String value, int defaultValue) {
         try {
@@ -14,6 +18,17 @@ public class ParseUtil {
             return Boolean.parseBoolean(value);
         } catch (NumberFormatException e) {
             return defaultValue;
+        }
+    }
+
+    public static Timestamp tryParseDateTime(String date, String time) {
+        try {
+            String dateTimeStr = date + " " + time;
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            Date parseDate = dateFormat.parse(dateTimeStr);
+            return new Timestamp(parseDate.getTime());
+        } catch (Exception e) {
+            return null;
         }
     }
 }
