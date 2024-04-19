c4afd05d609843383eca66ccb3318ca9d6aa8ccb
alexchoicy <alex123321choicy@gmail.com>
2024-04-20 02:04:53 +0800

    get Location

--- src/main/java/com/cems/database/LocationManager.java
+++ src/main/java/com/cems/database/LocationManager.java
@@ -3,6 +3,7 @@ package com.cems.database;
 import com.cems.Model.Location;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -27,5 +28,28 @@ public class LocationManager extends DatabaseManager{
             e.printStackTrace();
         }
         return locations;
+    }
+
+    public Location getLocation(int locationID) {
+        try {
+            String query = "SELECT * FROM location WHERE location_ID = ?";
+            try (Connection connect = getConnection()) {
+                PreparedStatement statement = connect.prepareStatement(query);
+
+                statement.setInt(1, locationID);
+
+                ResultSet resultSet = statement.executeQuery();
+
+                if (resultSet.next())  {
+                    Location location = new Location();
+                    location.setId(resultSet.getInt("location_id"));
+                    location.setName(resultSet.getString("location_name"));
+                    return location;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
