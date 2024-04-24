06860f1195241142bcd2962ff932b76d977109c6
alexchoicy <alex123321choicy@gmail.com>
2024-04-24 12:57:37 +0800

    add status on reservation item

---
+++ src/main/java/com/cems/Enums/ReservationItemStatus.java
@@ -0,0 +1,28 @@
+package com.cems.Enums;
+
+import java.util.HashMap;
+
+public enum ReservationItemStatus {
+    NEED_DELIVERY(1),
+    ARRIVED(2);
+
+    private final int value;
+
+    private static final HashMap<Integer, ReservationItemStatus> map = new HashMap<>();
+
+    static {
+        for (ReservationItemStatus status : ReservationItemStatus.values()) {
+            map.put(status.value, status);
+        }
+    }
+
+    ReservationItemStatus(int value) {
+        this.value = value;
+    }
+
+    public static ReservationItemStatus getStatus(int status) { return map.get(status);}
+
+    public int getValue() {
+        return value;
+    }
+}
